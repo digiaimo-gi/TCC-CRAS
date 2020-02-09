94063290bd261581de983fd26b10d15d4dbce9dd
Giovanna Di Giaimo <gio.dg.silva@gmail.com>
2020-02-09 13:59:26 -0300

    inicio na view index de rh

--- resources/views/recursos_humanos/index.blade.php
+++ resources/views/recursos_humanos/index.blade.php
@@ -3,5 +3,38 @@
 @section('title', 'Recursos Humanos - Início')
  
 @section('content')
-Recursos Humanos - Início
+<div class="title">
+    <h3>Funcionários</h3>
+</div>
+
+<div class="botaoAdd">
+    <a href="{{url('/recursosHumanos/create')}}" class="btn btn-success"><i class="fas fa-plus"></i> Novo</a>
+</div>
+
+<div class="subcontent">
+    <table class="table table-sm">
+        <thead>
+            <tr>
+                <th scope="col">Matrícula</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Data de Nascimento</th>
+                <th scope="col">Cargo</th>
+                <th scope="col" colspan="2">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($data['funcionario'] as $f)
+            <tr>
+                
+                <td>{{$f->matricula}}</td>
+                <td>{{$f->nome}}</td>
+                <td>{{$f->dataNascimento}}</td>
+                <td>{{$f->cargo}}</td>
+                <td><span style="font-size: 2em"><a href="{{url('/recursosHumanos/ficha/' . $f->id)}}"><i class="fas fa-address-card"></i></a></span></td>
+                <td><span style="font-size: 2em"><a href=""></a></span></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
